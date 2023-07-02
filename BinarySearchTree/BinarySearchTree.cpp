c6a88f921a0e4b2f14cf3c88663e38f20c1ba36e
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-07-02 13:15:08 +0700

    tambahan fungsi void postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -88,7 +88,24 @@ public:
 		}
 	}
 
-	
+	void preorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
+
+	void postorder(Node* ptr)
+	{
+
 	
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
