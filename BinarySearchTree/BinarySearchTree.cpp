1be2b8e7918cbd6c03f771f384b2498dc4c2cb0c
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-07-02 13:20:01 +0700

    jika (ptr != NULL) maka cout << ptr->info << " ";

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -95,7 +95,15 @@ public:
 			cout << "Tree is empty" << endl;
 			return;
 		}
-		
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
+
+	
 	
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
