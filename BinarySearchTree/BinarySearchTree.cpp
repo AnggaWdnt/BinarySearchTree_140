03c6b0b3aadcbf808f7fe193740e3076a0c057a4
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-07-02 13:10:26 +0700

    tambahan if dan else di void search

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -72,7 +72,7 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	}
-
+};
 	
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
