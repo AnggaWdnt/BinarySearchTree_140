c6f8b581050a05fc059bba2ec1b844e65e7a7d31
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-07-02 13:11:38 +0700

    tambahan fungsi void inorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -72,7 +72,10 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	}
-};
+
+	void inorder(Node* ptr)
+	{
+	
 	
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
